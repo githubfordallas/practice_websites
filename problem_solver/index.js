c347fca8d4df971f35ff315c1c6fddd5b3f635c0
githubfordallas <dallasplunkett@gmail.com>
2017-12-11 19:01:13 +0900

    Create index.js

---
+++ problem_solver/index.js
@@ -0,0 +1,57 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Follow My Problems</title>
+  <meta name="description" content="Follow along and try to solve some of my javascript problems" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="normalize.css">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+  <div class="wrapper">
+  </div>
+
+  <script src="scripts.js"></script>
+</body>
+</html>
+
+<!--
+
+<div class="module">
+  <p class="title comment">Title</p>
+  <ol>
+    <div>
+      <li>
+        <span class="line comment">
+          // Requirements
+        </span>
+      </li>
+      <li>
+        <span class="line comment">
+          <a onclick="mod.toggleDiv(7)">// Toggle Code...</a>
+        </span>
+      </li>
+    </div>
+
+    <div class="hide">
+      <li>
+        <span class="line">
+          console.log(code);
+        </span>
+      </li>
+    </div>
+
+    <div>
+      <li>
+        <span class="line comment">
+          // Result
+        </span>
+      </li>
+    </div>
+  </ol>
+</div>
+
+-->
